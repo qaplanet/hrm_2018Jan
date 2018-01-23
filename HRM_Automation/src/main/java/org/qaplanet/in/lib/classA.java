83ca5b7099941c2d99f805e0ca4173f12ebe80f9
qaplanet <qaplanet@gmail.com>
2018-01-23 11:04:04 +0530

    New Project

---
+++ HRM_Automation/src/main/java/org/qaplanet/in/lib/classA.java
@@ -0,0 +1,13 @@
+package org.qaplanet.in.lib;
+
+public class classA
+{
+	public void method1()
+	{
+		System.out.println("Method1");
+	}
+	public void method2()
+	{
+		System.out.println("Method2");
+	}
+}
---
+++ HRM_Automation/src/main/java/org/qaplanet/in/lib/classB.java
@@ -0,0 +1,12 @@
+package org.qaplanet.in.lib;
+
+public class classB {
+	public void openApp()
+	{
+		System.out.println("Open ");
+	}
+	public void LoginApp()
+	{
+		System.out.println("Open ");
+	}
+}
